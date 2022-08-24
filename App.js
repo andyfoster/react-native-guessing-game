857e35dcad81a81682c533904cf69bbce5ac3741
Andy Foster <andrewfosteris@gmail.com>
2022-08-24 13:22:57 +1200

    Create basic layout of Start screen and PrimaryButton

--- App.js
+++ App.js
@@ -1,44 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  const [currentTime, setCurrentTime] = useState('then');
-
-  function updateTime() {
-    setCurrentTime(Date.now());
-  }
-
   return (
-    <>
-      <View style={styles.topBar}>
-        <Text style={styles.headerText}>{currentTime}</Text>
-      </View>
-      <View style={styles.container}>
-        <Text>Guessing Game</Text>
-        <Button title="Time" onPress={updateTime} />
-        <StatusBar style="light" />
-      </View>
-    </>
+    <StartGameScreen />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  topBar: {
-    backgroundColor: 'blue',
-    height: 100,
-    padding: 5,
-    justifyContent: 'center',
-  },
-  headerText: {
-    color: 'white',
-    textAlign: 'center',
-    paddingTop: 30,
-  },
-});
+const styles = StyleSheet.create({});
