8e5213c79bb3f535dd0f9e5fcfbe1d371ad01b0f
Andy Foster <andrewfosteris@gmail.com>
2022-09-19 19:20:07 +1200

    Use Dimensions API on GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import { Image, Text, StyleSheet, View } from 'react-native';
+import { Image, Text, StyleSheet, View, Dimensions } from 'react-native';
 
 import * as Haptics from 'expo-haptics';
 
@@ -32,6 +32,8 @@ function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
 
 export default GameOverScreen;
 
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
@@ -40,9 +42,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: deviceWidth > 380 ? 150 : 300,
+    height: deviceWidth > 380 ? 150 : 300,
+    borderRadius: deviceWidth > 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
