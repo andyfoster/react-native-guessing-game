220e0d86e05cae96c4406bd49cb481272470404e
Andy Foster <andrewfosteris@gmail.com>
2022-09-21 22:13:31 +1200

    Dynamic image size adjustments based on window dimensions

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,11 @@
-import { Image, Text, StyleSheet, View, Dimensions } from 'react-native';
+import {
+  Image,
+  Text,
+  ScrollView,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from 'react-native';
 
 import * as Haptics from 'expo-haptics';
 
@@ -9,32 +16,53 @@ import PrimaryButton from '../components/ui/PrimaryButton';
 function GameOverScreen({ roundsNumber, userNumber, onStartNewGame }) {
   Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
 
+  const { width, height } = useWindowDimensions();
+
+  let imageSize = 300;
+
+  if (width < 380) {
+    imageSize = 150;
+  }
+
+  if (height < 430) {
+    imageSize = 130;
+  }
+
+  const imageStyleDynamic = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2,
+  };
+
   return (
-    <View style={styles.rootContainer}>
-      <Title>Game Over!</Title>
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={require('../assets/images/success.png')}
-        />
+    <ScrollView style={styles.screen}>
+      <View style={styles.rootContainer}>
+        <Title>Game Over!</Title>
+        <View style={[styles.imageContainer, imageStyleDynamic]}>
+          <Image
+            style={styles.image}
+            source={require('../assets/images/success.png')}
+          />
+        </View>
+        <View>
+          <Text style={styles.summaryText}>
+            You needed <Text style={styles.highlight}>{roundsNumber}</Text>{' '}
+            rounds to guess the number{' '}
+            <Text style={styles.highlight}>{userNumber}</Text>.
+          </Text>
+          <PrimaryButton onPress={onStartNewGame}>New Game</PrimaryButton>
+        </View>
       </View>
-      <View>
-        <Text style={styles.summaryText}>
-          You needed <Text style={styles.highlight}>{roundsNumber}</Text> rounds
-          to guess the number <Text style={styles.highlight}>{userNumber}</Text>
-          .
-        </Text>
-        <PrimaryButton onPress={onStartNewGame}>New Game</PrimaryButton>
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
-
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
   rootContainer: {
     flex: 1,
     padding: 24,
@@ -42,9 +70,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    width: deviceWidth > 380 ? 150 : 300,
-    height: deviceWidth > 380 ? 150 : 300,
-    borderRadius: deviceWidth > 380 ? 75 : 150,
     borderWidth: 3,
     borderColor: Colors.primary800,
     overflow: 'hidden',
@@ -63,6 +88,6 @@ const styles = StyleSheet.create({
   },
   highlight: {
     fontFamily: 'open-sans-bold',
-    color: Colors.primary500,
+    color: Colors.accent500,
   },
 });
